0196653bcb1a717a6d7e5cea975ae48a57a67878
Anushka <kulshres@grinnell.edu>
2019-02-21 22:00:54 -0600

    finish simulation set-up

--- src/ALifeSim.java
+++ src/ALifeSim.java
@@ -1,18 +1,26 @@
+
+
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.util.ConcurrentModificationException;
 
 public class ALifeSim {
 
-  public static void printReport(Pair<String,Integer>[] counts, int iterations) {
+  public static void printReport( int iterations,Population thisWorld) {
     PrintWriter pen = new PrintWriter(System.out, true);
-
+    
+    Pair<String,Integer>[] counts= thisWorld.getPopulationCounts();
+    
+    DecimalFormat df = new DecimalFormat("#.##");    
+    
     pen.println("After " + iterations + " ticks:");
-    pen.println("Cooperators =" + counts[0].getRight());
-    pen.println("Defectors =" + counts[1].getRight());
-    pen.println("Partial Cooperators =" + counts[2].getRight());
+    pen.println("Cooperators = " + counts[0].getRight());
+    pen.println("Defectors = " + counts[1].getRight());
+    pen.println("Partial Cooperators = " + counts[2].getRight());
     pen.println("");
     pen.println("");
-    pen.println("Mean Cooperation Probability = ");
+    pen.println("Mean Cooperation Probability = "+ String.valueOf(df.format(
+              thisWorld.calculateCooperationMean())));
 
   }
 
@@ -28,19 +36,19 @@ public class ALifeSim {
     // java ALifeSim <#/iterations> <#/cooperators> <#/defectors> <#/partial cooperators>
 
     //the simulation runs
-    int coopNum= Integer.parseInt(args[1]);
-    int defectNum= Integer.parseInt(args[2]);
-    int partialNum= Integer.parseInt(args[3]);
+    int coopNum= Integer.parseInt(args[2]);
+    int defectNum= Integer.parseInt(args[3]);
+    int partialNum= Integer.parseInt(args[4]);
     Population thisWorld= new Population(map(coopNum, defectNum, partialNum));
     
-    int iterations= Integer.parseInt(args[0]);
+    int iterations= Integer.parseInt(args[1]);
     
     for(int i=0; i<iterations; i++) {
       thisWorld.update();
        }
-    Pair<String,Integer>[] counts= thisWorld.getPopulationCounts();
     
-    printReport(counts, iterations);
+    
+    printReport( iterations, thisWorld);
     
     
     //print result
